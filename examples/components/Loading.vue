b3b55bc9e4abc2ecd045ed441fc0d2ee49410cbd
Janking <315359131@qq.com>
2017-07-20 08:55:23 +0800

    update loading 1.0.0

--- examples/components/Loading.vue
+++ examples/components/Loading.vue
@@ -2,8 +2,13 @@
   <div class="markdown-body loading">
     <h1>加载动效组件</h1>
     <div class="loading__demo">
-      <button v-loading="{visible:loading}" class="btn" @click="toggle('loading')">全屏</button>
-      <button class="btn" v-loading="{context:'.loading__demo',visible:loading2}" @click="toggle('loading2')">局部</button>
+      <button v-loading="{visible:loading}" class="btn" @click="toggle('loading')">Fullscreen</button>
+      <button class="btn" v-loading="{context:'.loading__demo',visible:loading2}" @click="toggle('loading2')">Local</button>
+      <button class="btn" v-loading="{type:1,visible:loading3}" @click="toggle('loading3')">Loading Type 1</button>
+      <button class="btn" v-loading="{type:2,visible:loading4}" @click="toggle('loading4')">Loading Type 2</button>
+      <button class="btn" v-loading="{type:3,visible:loading5}" @click="toggle('loading5')">Loading Type 3</button>
+      <button class="btn" v-loading="{type:4,visible:loading6}" @click="toggle('loading6')">Loading Type 4</button>
+      <button class="btn" v-loading="{type:4,visible:loading7,text:'正在努力加载中'}" @click="toggle('loading7')">Loading Text</button>
     </div>
     <div v-html="doc"></div>
   </div>
@@ -20,7 +25,12 @@ export default {
     return {
       doc: doc,
       loading: false,
-      loading2: false
+      loading2: false,
+      loading3: false,
+      loading4: false,
+      loading5: false,
+      loading6: false,
+      loading7: false
     }
   },
   methods: {
@@ -30,7 +40,7 @@ export default {
       timer = setTimeout(function () {
         clearTimeout(timer)
         _this[name] = !_this[name]
-      }, 3000)
+      }, 2000)
     }
   }
 }
@@ -42,6 +52,10 @@ export default {
   #{&}__demo {
     text-align: center;
     padding: rem(100) 0;
+    .btn {
+      display: block;
+      margin-bottom: rem(40)
+    }
   }
 }
 </style>
--- src/js-component/loading/index.js
+++ src/js-component/loading/index.js
@@ -2,26 +2,27 @@ import './loading.scss'
 import Vue from 'vue'
 import iuimLoading from './loading.vue'
 let LoadingConstructor = Vue.extend(iuimLoading)
-
 export default {
   install: function (Vue) {
     if (this.installed) return
-    let contextEl
+
     let config = {
       bind(el, binding) {
-        let defaults = { fullscreen: false, text: false, context: 'body', visible: false }
+        let defaults = { fullscreen: false, context: 'body', visible: false, text: false, type: 1 }
         let options = typeof binding.value === 'object' ? binding.value : {}
         let setting = Object.assign({}, defaults, options)
+
         Vue.nextTick(() => {
           let loading = new LoadingConstructor({
             el: document.createElement('div'),
             data: setting
           })
-          contextEl = document.querySelector(setting.context)
+          let contextEl = document.querySelector(setting.context)
+          el.contextEl = contextEl
           contextEl.appendChild(loading.$el)
           el.instance = loading
           loading.$on('after-leave', () => {
-            contextEl.classList.remove('iui-loading-wrap')
+            contextEl.classList.remove('iuim-loading-wrap')
           })
         })
       },
@@ -30,16 +31,14 @@ export default {
         if (binding.oldValue !== binding.value) {
           loading.visible = binding.value.visible
           if (loading.context !== 'body' && binding.value.visible) {
-            contextEl.classList.add('iui-loading-wrap')
+            el.contextEl.classList.add('iuim-loading-wrap')
           }
         }
       },
       unbind(el, binding) {
         el.instance.$el.remove()
-        console.log(el)
       }
     }
-
     Vue.directive('loading', config)
   }
 }
