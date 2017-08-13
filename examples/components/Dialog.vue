7134d1200a43bab792b0b30ef73d776e963eff3e
聂杞锋 <308561157@qq.com>
2017-08-13 18:33:10 +0800

    update dialog

--- examples/components/Dialog.vue
+++ examples/components/Dialog.vue
@@ -2,26 +2,38 @@
   <div class="markdown-body">
     <h1>对话框</h1>
     <div class="dialog__demo">
-      <button class="btn" v-dialog="{visible:dialog}" @click="toggle('dialog')">default</button>
+      <!-- <button class="btn" v-dialog="{visible:dialog}" @click="toggle('dialog')">default</button> -->
+      <button class="btn" @click="open('iuimDialog',{message:'Hey, I am Opened, but I dont have title',type:'default'})">default</button>
+      <iuimDialog ref="iuimDialog"></iuimDialog>
     </div>
   </div>
 </template>
 
 <script>
-import dialog from 'js-component/dialog'
-import Vue from 'vue'
-Vue.use(dialog)
+// import dialog from 'js-component/dialog'
+import iuimDialog from 'js-component/dialog/dialog.vue'
+// import Vue from 'vue'
+// Vue.use(dialog)
 export default {
+  components: {
+    iuimDialog
+  },
   data() {
     return {
-      dialog: false
+      // dialog: false
     }
   },
   methods: {
     toggle(name) {
       let _this = this
       _this[name] = !_this[name]
+    },
+    open (plugin, options) {
+      this.$refs[plugin].openDialog(options)
     }
+  },
+  mounted: function() {
+    // console.log(this)
   }
 }
 </script>
--- src/js-component/dialog/dialog.vue
+++ src/js-component/dialog/dialog.vue
@@ -1,15 +1,52 @@
 <template>
-  <div class="iuim-dialog" v-if="visible">
-    <div class="dialog-box">
+  <!-- <div class="iuim-dialog" v-if="visible"> -->
+  <div class="iuim-dialog">
+    <div class="dialog-box" :class="classDialog">
       <div class="dialog-content">
         <div class="dialog-body">
           <div>Hey, I am Opened, but I dont have title</div>
         </div>
         <div class="dialog-footer">
-          <button class="btn">Close</button>
+          <button class="btn" @click="closeDialog()">Close</button>
         </div>
       </div>
     </div>
-    <div class="dialog-backdrop"></div>
+    <div class="dialog-backdrop" :class="classDialog"></div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'iuimDialog',
+
+  data: function () {
+    return {
+      showDialog: false
+    }
+  },
+
+  computed: {
+    classDialog: function () {
+      var clasz = ''
+      if (this.showDialog) {
+        clasz = 'show'
+      }
+      return clasz
+    }
+  },
+
+  methods: {
+    openDialog: function (options) {
+      var _self = this
+      _self.showDialog = true
+    },
+    closeDialog: function () {
+      var _self = this
+      _self.showDialog = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./dialog.scss"></style>
+
