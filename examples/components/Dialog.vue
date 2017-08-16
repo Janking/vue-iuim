7170ecd759787814f422bdca3b9bc3ace31eabc3
聂杞锋 <308561157@qq.com>
2017-08-16 19:48:48 +0800

    update dialog 8-16

--- examples/components/Dialog.vue
+++ examples/components/Dialog.vue
@@ -1,40 +1,133 @@
 <template>
-  <div class="markdown-body">
-    <h1>对话框</h1>
+  <div class="markdown-body dialog">
+    <h2>对话框</h2>
+    <h2>Examples</h2>
     <div class="dialog__demo">
-      <!-- <button class="btn" v-dialog="{visible:dialog}" @click="toggle('dialog')">default</button> -->
-      <button class="btn" @click="open('iuimDialog',{message:'Hey, I am Opened, but I dont have title',type:'default'})">default</button>
-      <iuimDialog ref="iuimDialog"></iuimDialog>
+      <button class="btn" @click="open(defaultOption)">default</button>
+      <button class="btn" @click="open(infoOption)">info</button>
+      <button class="btn" @click="open(confirmOption)">confirm</button>
+      <button class="btn" @click="open(onOpenOption)">onOpen</button>
+      <button class="btn" @click="open(onCloseOption)">onClose</button>
+      <button class="btn" @click="open(overlayOption)">overlay</button>
     </div>
+    <div class="dialog__demo">
+      <button class="btn" @click="open(zoomOption)">zoom</button>
+      <button class="btn" @click="open(rotateOption)">rotate</button>
+      <button class="btn" @click="open(fadeOption)">fade</button>
+      <button class="btn" @click="open(flipOption)">flip</button>
+      <button class="btn" @click="open(doorOption)">door</button>
+      <button class="btn" @click="open(slideUpOption)">slideUp</button>
+    </div>
+    <iuimDialog ref="iuimDialog"></iuimDialog>
   </div>
 </template>
 
 <script>
-// import dialog from 'js-component/dialog'
 import iuimDialog from 'js-component/dialog/dialog.vue'
-// import Vue from 'vue'
-// Vue.use(dialog)
 export default {
   components: {
     iuimDialog
   },
   data() {
     return {
-      // dialog: false
+      defaultOption: {
+        plugin: 'iuimDialog',
+        message: 'Hey, I am Opened, but I dont have title',
+        type: 'default'
+      },
+      infoOption: {
+        plugin: 'iuimDialog',
+        message: 'Hey, I am Opened, but I dont have title',
+        type: 'info'
+      },
+      confirmOption: {
+        plugin: 'iuimDialog',
+        message: 'Hey, I am Opened, but I dont have title',
+        type: 'confirm',
+        onConfirm: function () {
+          console.log('Hey, You Confirmed')
+        }
+      },
+      onOpenOption: {
+        plugin: 'iuimDialog',
+        message: 'There should be an alert before you see this',
+        onOpen: function () {
+          console.log('Hey, I am called when alert is opened')
+        }
+      },
+      onCloseOption: {
+        plugin: 'iuimDialog',
+        message: 'Click close to trigger custom function',
+        onClose: function () {
+          console.log('Hey, I am Close Function')
+        }
+      },
+      overlayOption: {
+        plugin: 'iuimDialog',
+        message: 'Now You can not close me by clicking overlay',
+        disableOverlayClick: true
+      },
+
+      zoomOption: {
+        plugin: 'iuimDialog',
+        message: 'zoom-enter',
+        type: 'confirm',
+        animateClass: 'zoom'
+      },
+      rotateOption: {
+        plugin: 'iuimDialog',
+        message: 'rotate-enter',
+        type: 'confirm',
+        animateClass: 'rotate'
+      },
+      fadeOption: {
+        plugin: 'iuimDialog',
+        message: 'fade-enter',
+        type: 'confirm',
+        animateClass: 'fade'
+      },
+      flipOption: {
+        plugin: 'iuimDialog',
+        message: 'flip-enter',
+        type: 'confirm',
+        animateClass: 'flip'
+      },
+      doorOption: {
+        plugin: 'iuimDialog',
+        message: 'door-enter',
+        type: 'confirm',
+        animateClass: 'door'
+      },
+      slideUpOption: {
+        plugin: 'iuimDialog',
+        message: 'slideUp-enter',
+        type: 'confirm',
+        animateClass: 'slideUp'
+      }
     }
   },
   methods: {
-    toggle(name) {
-      let _this = this
-      _this[name] = !_this[name]
-    },
-    open (plugin, options) {
-      this.$refs[plugin].openDialog(options)
+    open (options) {
+      this.$refs[options.plugin].openDialog(options)
     }
-  },
-  mounted: function() {
-    // console.log(this)
   }
 }
 </script>
 
+<style lang="scss">
+@import '~scss-kit';
+.dialog {
+  #{&}__demo {
+    text-align: center;
+    padding: rem(100) 0;
+    border: 5px #7dc1f1 dashed;
+    width: 49%;
+    display: inline-block;
+    vertical-align: top;
+    .btn {
+      display: block;
+      margin-bottom: rem(40)
+    }
+  }
+}
+</style>
--- src/js-component/dialog/dialog.vue
+++ src/js-component/dialog/dialog.vue
@@ -1,17 +1,19 @@
 <template>
-  <!-- <div class="iuim-dialog" v-if="visible"> -->
   <div class="iuim-dialog">
-    <div class="dialog-box" :class="classDialog">
-      <div class="dialog-content">
-        <div class="dialog-body">
-          <div>Hey, I am Opened, but I dont have title</div>
-        </div>
-        <div class="dialog-footer">
-          <button class="btn" @click="closeDialog()">Close</button>
+    <transition :name="animateClass">
+      <div class="dialog-box" :class="classBox" @click="closeOverlay" v-if="showDialog">
+        <div class="dialog__content dialog__content--radius">
+          <div class="dialog-body">
+            <div v-html="message"></div>
+          </div>
+          <div class="dialog-footer" v-if="!hideAllButton">
+            <button class="btn" v-if="useConfirmBtn" @click="whenConfirm">Confirm</button>
+            <button class="btn" @click="closeDialog">Close</button>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="dialog-backdrop" :class="classDialog"></div>
+    </transition>
+    <div class="dialog-backdrop" :class="classBackdrop"></div>
   </div>
 </template>
 
@@ -21,32 +23,125 @@ export default {
 
   data: function () {
     return {
-      showDialog: false
+      showDialog: false,
+      message: '',
+      hideAllButton: false,
+      useConfirmBtn: false,
+      onConfirm: null,
+      onOpen: null,
+      onClose: null,
+      disableOverlayClick: false,
+      animateClass: '',
+      customClass: ''
     }
   },
 
   computed: {
-    classDialog: function () {
-      var clasz = ''
+    classBox: function () {
+      let clasz = this.customClass
       if (this.showDialog) {
-        clasz = 'show'
+        clasz = this.customClass + 'dialog--shown'
+      }
+      return clasz
+    },
+    classBackdrop: function () {
+      let clasz = this.customClass
+      if (this.showDialog) {
+        clasz = this.customClass + 'dialog--shown dialog-fade-enter'
+      } else {
+        clasz = 'dialog-fade-leave'
       }
       return clasz
     }
   },
 
   methods: {
-    openDialog: function (options) {
-      var _self = this
-      _self.showDialog = true
+    closeOverlay: function (event) {
+      let _self = this
+
+      if (event.target.className.indexOf('dialog--shown') > 0 && !_self.disableOverlayClick) {
+        _self.showDialog = false
+      }
     },
-    closeDialog: function () {
-      var _self = this
+
+    whenConfirm: function (event) {
+      let _self = this
+      event.preventDefault()
+
       _self.showDialog = false
+
+      if (_self.onConfirm !== null) {
+        _self.onConfirm()
+      }
+    },
+
+    openDialog: function (options) {
+      let _self = this
+
+      if (options.type === 'info') {
+        _self.hideAllButton = true
+      } else {
+        _self.hideAllButton = false
+      }
+
+      if (typeof options.animateClass !== 'undefined') {
+        _self.animateClass = options.animateClass
+      } else {
+        _self.animateClass = ''
+      }
+
+      if (options.type === 'confirm') {
+        _self.useConfirmBtn = true
+      } else {
+        _self.useConfirmBtn = false
+      }
+
+      if (typeof options.disableOverlayClick !== 'undefined') {
+        _self.disableOverlayClick = options.disableOverlayClick
+      } else {
+        _self.disableOverlayClick = false
+      }
+
+      if (typeof options.onConfirm !== 'undefined' && options.onConfirm !== null) {
+        _self.onConfirm = options.onConfirm
+      } else {
+        _self.onConfirm = null
+      }
+
+      if (typeof options.onClose !== 'undefined' && options.onClose !== null) {
+        _self.onClose = options.onClose
+      } else {
+        _self.onClose = null
+      }
+
+      if (typeof options.onOpen !== 'undefined') {
+        _self.onOpen = options.onOpen
+      } else {
+        _self.onOpen = null
+      }
+
+      if (_self.onOpen !== null) {
+        _self.onOpen()
+      }
+
+      _self.showDialog = true
+      _self.message = options.message
+    },
+
+    closeDialog: function (event) {
+      let _self = this
+      event.preventDefault()
+
+      _self.showDialog = false
+
+      if (_self.onClose !== null) {
+        _self.onClose()
+      }
     }
   }
+
 }
 </script>
 
+<style lang="scss" src="./animate.scss"></style>
 <style lang="scss" src="./dialog.scss"></style>
-
